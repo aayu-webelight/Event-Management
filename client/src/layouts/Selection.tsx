71713eaaad7bdf28c9430fe008b75656c907465b
Aayu Agrawal <aayua@webelight.co.in>
2022-12-05 16:32:13 +0530

    Click Away

--- client/src/layouts/Selection.tsx
+++ client/src/layouts/Selection.tsx
@@ -4,6 +4,7 @@ import Grid from "@mui/material/Grid";
 import AddShow from "../Components/AddShow";
 import Typography from "@mui/material/Typography";
 import { List, ListItemButton } from "@mui/material";
+import ClickAwayListener from "@mui/material/ClickAwayListener";
 import DeleteDialog from "../Components/DeleteDialog";
 import IBooking from "../interfaces/Booking";
 import AddBooking from "../Components/AddBooking";
@@ -41,11 +42,14 @@ const Selection = () => {
       });
   };
 
+  const handleClickAway = () => {
+    setChangeBookingStatus(false);
+  };
+
   const handleSelected = (event: IBooking) => {
     setSelectedEvent(event);
     setChangeBookingStatus(!changeBookingStatus);
   };
-
   return (
     <>
       {events.length ? (
@@ -58,94 +62,104 @@ const Selection = () => {
             </Box>
 
             <Box sx={{ flexGrow: 1 }}>
-              <Grid container spacing={2}>
-                {events.map((event: IBooking, key: number) => {
-                  const date = new Date(event.dateTime);
-                  return (
-                    <Grid
-                      key={key}
-                      xs={2}
-                      margin={1}
-                      onClick={() => handleSelected(event)}
-                      border={"1px solid black"}
-                      borderRadius={"15px"}
-                      style={{ paddingLeft: "0 !important" }}
-                      mt={0}
-                      pl={0}
-                    >
-                      <Typography sx={{ fontWeight: "bold" }} pl={2} pt={2}>
-                        Name:
-                      </Typography>
-                      <Typography pl={2}>{event.showName} </Typography>
-                      <hr></hr>
-                      <Typography pl={2} sx={{ fontWeight: "bold" }}>
-                        Date of Event:
-                      </Typography>
-                      <Typography pl={2}>
-                        {date.getDate()}-{date.getMonth() + 1}-
-                        {date.getFullYear()}
-                      </Typography>
-                      <hr></hr>
-                      <Typography pl={2} sx={{ fontWeight: "bold" }}>
-                        Time of Event:
-                      </Typography>
-                      <Typography pl={2}>
-                        {date.getHours()}:{date.getMinutes()}
-                      </Typography>
+              <ClickAwayListener onClickAway={handleClickAway}>
+                <Grid container spacing={2}>
+                  {events.map((event: IBooking, key: number) => {
+                    const date = new Date(event.dateTime);
+                    return (
+                      <Grid
+                        key={key}
+                        xs={2}
+                        margin={1}
+                        onClick={() => handleSelected(event)}
+                        border={"1px solid black"}
+                        borderRadius={"15px"}
+                        style={{ paddingLeft: "0 !important" }}
+                        mt={0}
+                        pl={0}
+                      >
+                        <Typography sx={{ fontWeight: "bold" }} pl={2} pt={2}>
+                          Name:
+                        </Typography>
+                        <Typography pl={2}>{event.showName} </Typography>
+                        <hr></hr>
+                        <Typography pl={2} sx={{ fontWeight: "bold" }}>
+                          Date of Event:
+                        </Typography>
+                        <Typography pl={2}>
+                          {date.getDate()}-{date.getMonth() + 1}-
+                          {date.getFullYear()}
+                        </Typography>
+                        <hr></hr>
+                        <Typography pl={2} sx={{ fontWeight: "bold" }}>
+                          Time of Event:
+                        </Typography>
+                        <Typography pl={2}>
+                          {date.getHours()}:{date.getMinutes()}
+                        </Typography>
 
-                      {event.isBooked ? (
-                        <>
-                          <hr></hr>
-                          <Typography pl={2} sx={{ fontWeight: "bold" }}>
-                            Booked By:
-                          </Typography>
-                          <Typography pl={2}>{event.bookerName}</Typography>
-                        </>
-                      ) : (
-                        <></>
-                      )}
-                      {selectedEvent._id === event._id ? (
-                        <>
-                          <hr style={{ margin: "0" }}></hr>
-                          {event.isBooked ? (
-                            <List>
-                              <ListItemButton
-                                onClick={() => setShowCancelDialog(true)}
-                              >
-                                Cancel Booking
-                              </ListItemButton>
-                              <hr></hr>
-                              <ListItemButton
-                                onClick={() => setShowDeleteDialog(true)}
-                              >
-                                Delete Show
-                              </ListItemButton>
-                            </List>
-                          ) : (
-                            <>
-                              <List>
-                                <ListItemButton
-                                  onClick={() => setShowAddDialog(true)}
-                                >
-                                  Add Booking
-                                </ListItemButton>
-                                <hr></hr>
-                                <ListItemButton
-                                  onClick={() => setShowDeleteDialog(true)}
-                                >
-                                  Delete Show
-                                </ListItemButton>
-                              </List>
-                            </>
-                          )}
-                        </>
-                      ) : (
-                        <></>
-                      )}
-                    </Grid>
-                  );
-                })}
-              </Grid>
+                        {event.isBooked ? (
+                          <>
+                            <hr></hr>
+                            <Typography pl={2} sx={{ fontWeight: "bold" }}>
+                              Booked By:
+                            </Typography>
+                            <Typography pl={2}>{event.bookerName}</Typography>
+                          </>
+                        ) : (
+                          <></>
+                        )}
+                        {changeBookingStatus ? (
+                          <>
+                            {selectedEvent._id === event._id ? (
+                              <>
+                                <hr style={{ margin: "0" }}></hr>
+                                {event.isBooked ? (
+                                  <List>
+                                    <ListItemButton
+                                      onClick={() => setShowCancelDialog(true)}
+                                    >
+                                      Cancel Booking
+                                    </ListItemButton>
+                                    <hr></hr>
+                                    <ListItemButton
+                                      onClick={() => setShowDeleteDialog(true)}
+                                    >
+                                      Delete Show
+                                    </ListItemButton>
+                                  </List>
+                                ) : (
+                                  <>
+                                    <List>
+                                      <ListItemButton
+                                        onClick={() => setShowAddDialog(true)}
+                                      >
+                                        Add Booking
+                                      </ListItemButton>
+                                      <hr></hr>
+                                      <ListItemButton
+                                        onClick={() =>
+                                          setShowDeleteDialog(true)
+                                        }
+                                      >
+                                        Delete Show
+                                      </ListItemButton>
+                                    </List>
+                                  </>
+                                )}
+                              </>
+                            ) : (
+                              <></>
+                            )}
+                          </>
+                        ) : (
+                          <></>
+                        )}
+                      </Grid>
+                    );
+                  })}
+                </Grid>
+              </ClickAwayListener>
             </Box>
             <DeleteDialog
               showDeleteDialog={showDeleteDialog}
